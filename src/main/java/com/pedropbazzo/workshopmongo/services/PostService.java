1b6f4202a5115ed6142c60ae7871520218c287e1
“pedropbazzo” <you@example.com>
2022-01-15 20:31:56 -0300

    first commit

---
+++ src/main/java/com/pedropbazzo/workshopmongo/services/PostService.java
@@ -0,0 +1,35 @@
+package com.pedropbazzo.workshopmongo.services;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pedropbazzo.workshopmongo.domain.Post;
+import com.pedropbazzo.workshopmongo.repository.PostRepository;
+import com.pedropbazzo.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Post user = repo.findOne(id);
+		if (user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
+	}
+	
+	public List<Post> findByTitle(String text) {
+		return repo.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
+	}
+}
---
+++ src/main/java/com/pedropbazzo/workshopmongo/services/UserService.java
@@ -0,0 +1,54 @@
+package com.pedropbazzo.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pedropbazzo.workshopmongo.domain.User;
+import com.pedropbazzo.workshopmongo.dto.UserDTO;
+import com.pedropbazzo.workshopmongo.repository.UserRepository;
+import com.pedropbazzo.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		User user = repo.findOne(id);
+		if (user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		repo.delete(id);
+	}
+	
+	public User update(User obj) {
+		User newObj = repo.findOne(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+}
